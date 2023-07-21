35b909a6fc479b1b0c01ec122305aad71b886753
gadji <gadji180105@gmail.com>
2023-07-21 11:39:58 +0300

    add media mobile nav

--- scripts/main.js
+++ scripts/main.js
@@ -3,5 +3,5 @@ const headerBtn = document.querySelector('.header__button-nav');
 
 headerBtn.addEventListener('click', function() {
     headerNav.classList.toggle('header__nav_open');
-    headerBtn.classList.toggle('.header__button-nav active');
+    headerBtn.classList.toggle('header__button-nav_active');
 });
