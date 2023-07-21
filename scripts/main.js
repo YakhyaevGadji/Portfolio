bbdf4d2d22216b550a81c95056f33e84201b19e7
gadji <gadji180105@gmail.com>
2023-07-21 10:22:14 +0300

    rollback commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,7 @@
-console.log(true);
+const headerNav = document.querySelector('.header__nav');
+const headerBtn = document.querySelector('.header__button-nav');
+
+headerBtn.addEventListener('click', function() {
+    headerNav.classList.toggle('header__nav_open');
+    headerBtn.classList.toggle('.header__button-nav active');
+});
