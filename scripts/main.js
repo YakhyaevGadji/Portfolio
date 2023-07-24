3044a67f8ce09c8c216c58d0fe27a4d2580b6935
gadji <gadji180105@gmail.com>
2023-07-24 11:03:30 +0300

    fixed media mobile

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,10 @@
-const headerNav = document.querySelector('.header__nav');
 const headerBtn = document.querySelector('.header__button-nav');
+const headerNav = document.querySelector('.header__nav');
+const body = document.querySelector('body');
+
 
 headerBtn.addEventListener('click', function() {
-    headerNav.classList.toggle('header__nav_open');
     headerBtn.classList.toggle('header__button-nav_active');
+    headerNav.classList.toggle('header__nav_active');
+    body.classList.toggle('lock');
 });
