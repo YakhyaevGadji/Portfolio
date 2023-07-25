993fdcd1d329ba1f8415228892099338fb132abe
gadji <gadji180105@gmail.com>
2023-07-25 09:12:00 +0300

    add animations

---
+++ scripts/animation.js
@@ -0,0 +1,19 @@
+function onEntry(entry) {
+    entry.forEach(change => {
+        if (change.isIntersecting) {
+            change.target.classList.add('elem-active');
+        }
+    });
+}
+  
+let options = {
+    threshold: [0.5]
+};
+
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll('.anim');
+
+for (let elm of elements) {
+    observer.observe(elm);
+}
+
